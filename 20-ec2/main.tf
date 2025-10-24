870f03abd44271f3da5f64a664a898c0aed362a5
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-24 16:39:36 +0530

    fix

--- 20-ec2/main.tf
+++ 20-ec2/main.tf
@@ -1,10 +1,10 @@
 module "this" {  
   source = "git::https://github.com/Sathishdevops38/terraform-modules.git//ec2_module?ref=main"
-  instance_type = var.instance_type
-  sg_ids =   [var.sg_id]
   num_instance = var.instance_num
   project_name = var.project_name
   environment = var.env
+  instance_type = var.instance_type
+  sg_ids =   [var.sg_id]
   tags= {
     terraform = true
   }
