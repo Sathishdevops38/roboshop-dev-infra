31bb8b0b589a3a3e01e710eea25c6c763b1b8025
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-28 11:28:04 +0530

    alb

---
+++ 30-alb/main.tf
@@ -0,0 +1,18 @@
+module "frontend_alb" {
+  source = "git::https://github.com/Sathishdevops38/terraform-modules.git//30-alb-module?ref=main"
+  name = local.alb_name
+  project_name = var.project_name
+  environment = var.environment
+  security_groups = local.frontend_lb_sg_id
+  subnets = local.public_subnet_id
+  alb_tags = var.alb_tags
+  load_balancer_type = var.load_balancer_type
+
+  #tg config
+  tg_name = local.tg_name
+  target_type = var.target_type
+  tg_port = var.port
+  tg_protocol = var.protocol
+  vpc_id = local.vpc_id
+  alb_tg_tags = var.alb_tg_tags
+}
---
+++ 30-alb/variables.tf
@@ -0,0 +1,37 @@
+variable "project_name" {
+  type = string
+  default = "roboshop"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "alb_tags" {
+  default = {
+    created_by = "terraform"
+  }
+}
+
+variable "load_balancer_type" {
+  default = "application"
+}
+
+variable "target_type" {
+  default = "alb"
+}
+
+variable "port" {
+  default = 80
+}
+
+variable "protocol" {
+  default = "TCP"
+}
+
+variable "alb_tg_tags"{
+  default = {
+    created_by = "terraform"
+  }
+}
