268dc6b74bfc9e532320c494cfa6b68f57f67197
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-30 11:13:12 +0530

    r53

--- 50-databases/variables.tf
+++ 50-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z05700624Q21KABEP00F"
+}
+
+variable "domain_name" {
+  default = "daws38sat.fun"
+}
