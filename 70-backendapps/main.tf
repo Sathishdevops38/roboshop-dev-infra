d544e89b6e8ceb5a9988aa575c17b92b5e60f622
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-04 10:50:55 +0530

    backend

---
+++ 70-backendapps/main.tf
@@ -0,0 +1,23 @@
+# Root Configuration main.tf
+
+# -----------------------------------------------------------------------------
+# 1. Module Call: Deploy multiple components using for_each
+# -----------------------------------------------------------------------------
+module "roboshop_components" {
+  # Iterates through the map defined in var.components (catalogue, user, shipping)
+  for_each = var.components
+  
+  source = "git::https://github.com/Sathishdevops38/terraform-modules.git//40-backend-app?ref=main"
+  
+  # Pass common variables to the module
+  project_name = var.project_name
+  environment  = var.environment
+  aws_region   = var.aws_region
+  domain_name  = var.domain_name
+
+  # Pass the unique component configuration
+  component_name       = each.key                  # e.g., "catalogue"
+  instance_type        = each.value.instance_type  # e.g., "t3.micro"
+  application_port     = each.value.port           # e.g., 8080
+  health_check_path    = each.value.health_check_path # e.g., "/health"
+}
---
+++ 70-backendapps/variables.tf
@@ -0,0 +1,43 @@
+variable "components" {
+  description = "Map of components to deploy with specific settings."
+  type = map(object({
+    instance_type       = string
+    port                = number
+    health_check_path   = string
+  }))
+  default = {
+    "catalogue" = {
+      instance_type       = "t3.micro"
+      port                = 8080
+      health_check_path   = "/"
+    }
+    "user" = {
+      instance_type       = "t3.small"
+      port                = 8080
+      health_check_path   = "/"
+    }
+  #   "shipping" = {
+  #     instance_type       = "t3.micro"
+  #     port                = 8080
+  #     health_check_path   = "/"
+  #   }
+  }
+}
+
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "domain_name" {
+  default = "daws38sat.fun"
+}
+
