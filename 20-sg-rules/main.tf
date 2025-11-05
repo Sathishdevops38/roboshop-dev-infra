fdf0ee3ed9123220b47636801edea4371c89b022
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-05 11:57:20 +0530

    data

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -98,6 +98,14 @@ resource "aws_security_group_rule" "shipping_bastion" {
   protocol          = "tcp"
   to_port           = 22
 }
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  security_group_id = local.frontend_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
 
 resource "aws_security_group_rule" "catalogue_backend_alb" {
   type              = "ingress"
--- 90-frontend-app/data.tf
+++ 90-frontend-app/data.tf
@@ -13,8 +13,8 @@ data "aws_ami" "ami_2023" {
   }
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
 
 data "aws_ssm_parameter" "frontend_sg_id" {
