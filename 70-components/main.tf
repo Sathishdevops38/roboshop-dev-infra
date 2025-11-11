5623cf59b0f38e5cd1284ebfbcf51992fed0fb70
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-11 13:57:57 +0530

    source

--- 70-components/main.tf
+++ 70-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "git::https://github.com/Sathishdevops38/terraform-modules.git//70-backend-apps?ref=main"
+    source = "git::https://github.com/Sathishdevops38/terraform-modules.git//40-backend-app?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
